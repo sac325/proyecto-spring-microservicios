7bd16b6b38e4ceb3fe4afd5acdd4ed5fe45799ef
Sergio <sac325@gmail.com>
2023-02-13 01:26:07 -0300

    RESUELTO CORS

--- springboot-servicio-gateway-server/src/main/java/com/quiniela/springbootserviciogatewayserver/filters/security/SpringSecurityConfig.java
+++ springboot-servicio-gateway-server/src/main/java/com/quiniela/springbootserviciogatewayserver/filters/security/SpringSecurityConfig.java
@@ -1,11 +1,17 @@
 package com.quiniela.springbootserviciogatewayserver.filters.security;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CorsSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @EnableWebFluxSecurity
 public class SpringSecurityConfig {
@@ -29,4 +35,17 @@ public class SpringSecurityConfig {
                 .build();
     }
     
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
 }
